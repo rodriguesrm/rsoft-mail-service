52bf7d2b397d1e77efcd9fdd230decfd8d0fd55b
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2021-05-15 06:18:25 -0300

    - Update to .NET 5 - Create Grpc Host

---
+++ src/RSoft.Mail.Web.Grpc/Services/SenderService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using RSoft.Mail.Web.Grpc.Contracts;
+using System.Threading.Tasks;
+
+namespace RSoft.Mail.Web.Grpc.Services
+{
+    public class SenderService : MailSender.MailSenderBase
+    {
+
+        /// <summary>
+        /// Send mail
+        /// </summary>
+        /// <param name="request">Request data</param>
+        /// <param name="context">Server call context</param>
+        public override Task<SendReply> Send(SendRequest request, ServerCallContext context)
+        {
+            return base.Send(request, context);
+        }
+
+    }
+}
